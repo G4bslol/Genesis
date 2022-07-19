a10d47dd061712b0fb26ce98a58e1b7e7ae56e45
Gabriel Lucas <gabs123lucas@gmail.com>
2022-07-19 13:08:55 -0300

    Começando com HTML/CSS + JS

---
+++ trilha fundamentar/Pilotando com a DOM/scripts.js
@@ -0,0 +1,19 @@
+/*
+    * Qual usar?
+        • getElementById
+        • getElementsByClassName
+        • getElementsByTagName
+        • querySelector
+        • querySelectypeAll
+
+*/
+
+// Manipulando conteúdos
+
+const element = document.querySelector('h1')
+
+element.textContent += "Olá Dev's"
+
+console.log(element.textContent)
+
+
